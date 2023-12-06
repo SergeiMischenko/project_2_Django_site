0b2a74259ee73f3efa3f8adc56e1a0d89398626e
SergeiMischenko <main0serg@gmail.com>
2023-12-06 15:58:14 +0600

    added more templates

--- testdjangosite/funnytail/views.py
+++ testdjangosite/funnytail/views.py
@@ -2,15 +2,19 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 
 
-def index(request) -> HttpResponse:
+def index(request):
     return render(request, 'funnytail/index.html')
 
 
-def categories(request, category_id) -> HttpResponse:
+def about(request):
+    return render(request, 'funnytail/about.html')
+
+
+def categories(request, category_id):
     if category_id > 5:
         return redirect('category_id', 1, permanent=True)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {category_id}</p>")
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+    return render(request, 'funnytail/error404.html')
