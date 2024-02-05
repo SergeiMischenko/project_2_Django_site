28e9d12f3acf713b70c9af21cd946ef72df2f3b4
SergeiMischenko <main0serg@gmail.com>
2024-02-05 14:06:50 +0600

    added annotate with Count class in ORM

--- testdjangosite/funnytail/templatetags/cats_tags.py
+++ testdjangosite/funnytail/templatetags/cats_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 import funnytail.views as views
 from funnytail.models import Breed, TagPosts
 
@@ -7,10 +9,10 @@ register = template.Library()
 
 @register.inclusion_tag('funnytail/list_categories.html')
 def show_categories(category_selected=0):
-    categories = Breed.objects.all()
+    categories = Breed.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'categories': categories, 'category_selected': category_selected}
 
 
 @register.inclusion_tag('funnytail/list_tags.html')
 def show_all_tags():
-    return {'tags': TagPosts.objects.all()}
+    return {'tags': TagPosts.objects.annotate(total=Count("posts")).filter(total__gt=0)}
